39cd4ab685d83dfb8bb0dc7c875e5e5f96448680
qsmlab <sabyashachi.mishra@gmail.com>
2026-01-07 06:56:00 +0530

    Add files via upload

---
+++ misc.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml'>
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1' />
+<link rel='stylesheet' type='text/css' href='menu_style.css' />
+<title></title>
+
+</head>
+<body>
+<ul class="menu red">
+        <li><a href="index.php" title="">Home</a></li>
+     <li><a href="group.php" title="">Group</a></li>
+        <li><a href="pubs.php" title="" >Publications</a></li>
+        <li><a href="research.php" title="">Research</a></li>
+        <li><a href="teaching.php" title="">Teaching</a></li>
+        <li><a href="misc.php" title="">Miscellaneous</a></li>
+</ul>
+<CENTER>
+<br><br>
+<TABLE WIDTH=80% BORDER=0 CELLPADDING=4 CELLSPACING=3>
+<COL WIDTH=256*>
+<TR>
+<TD WIDTH=100% VALIGN=TOP>
+<UL>
+</UL>
+</UL>                   
+
+<UL>
+<BR> <BR>
+</UL>                   
+
+
+<a href="gpnews.php" title="">Group News</a></li> <BR><BR>
+<a href="gpres.php" title="">Group Resources</a></li><BR><BR>
+<a href="gppics.php" title="">Group Pictures</a></li><BR><BR>
+
+</TD>
+</TR>
+</TABLE>
+<P STYLE="margin-bottom: 0in"><BR>
+</P>
+</BODY>
+</HTML>
